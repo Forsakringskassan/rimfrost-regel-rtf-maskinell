ec2ee82e879f8ff97ef4e79885036f23f7a5162d
Nils Elveros <nils.elveros@tromb.com>
2026-08-27 13:26:23 +0200

    fix: update to new framework version

---
+++ src/main/java/se/fk/github/maskinellregelratttillforsakring/logic/RtfKompletteringSvarService.java
@@ -0,0 +1,65 @@
+package se.fk.github.maskinellregelratttillforsakring.logic;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import java.util.ArrayList;
+import java.util.UUID;
+import se.fk.rimfrost.framework.handlaggning.model.Handlaggning;
+import se.fk.rimfrost.framework.handlaggning.model.HandlaggningUpdate;
+import se.fk.rimfrost.framework.handlaggning.model.ImmutableHandlaggningUpdate;
+import se.fk.rimfrost.framework.handlaggning.model.ImmutableIdtyp;
+import se.fk.rimfrost.framework.handlaggning.model.ImmutableIndividYrkandeRoll;
+import se.fk.rimfrost.framework.handlaggning.model.ImmutableYrkande;
+import se.fk.rimfrost.framework.handlaggning.model.IndividYrkandeRoll;
+import se.fk.rimfrost.framework.regel.logic.KompletteringSvarServiceInterface;
+import se.fk.rimfrost.regel.rtf.maskinell.jaxrsspec.controllers.generatedsource.model.RtfKompletteringSvar;
+
+@ApplicationScoped
+public class RtfKompletteringSvarService implements KompletteringSvarServiceInterface<RtfKompletteringSvar>
+{
+   private static final String PERSONNUMMER_TYP_ID = "PERSONNUMMER";
+
+   @Override
+   public RtfKompletteringSvar readSvarData(Handlaggning handlaggning)
+   {
+      return new RtfKompletteringSvar();
+   }
+
+   @Override
+   public HandlaggningUpdate registerSvar(Handlaggning handlaggning, RtfKompletteringSvar svar)
+   {
+      var individYrkandeRoller = new ArrayList<IndividYrkandeRoll>(handlaggning.yrkande().individYrkandeRoller());
+
+      for (var personnummer : svar.getPersonnummer())
+      {
+         individYrkandeRoller.add(createIndividYrkandeRoll(personnummer));
+      }
+
+      var uppdateratYrkande = ImmutableYrkande.builder()
+            .from(handlaggning.yrkande())
+            .individYrkandeRoller(individYrkandeRoller)
+            .build();
+
+      return ImmutableHandlaggningUpdate.builder()
+            .id(handlaggning.id())
+            .version(handlaggning.version())
+            .yrkande(uppdateratYrkande)
+            .processInstansId(handlaggning.processInstansId())
+            .skapadTS(handlaggning.skapadTS())
+            .avslutadTS(handlaggning.avslutadTS())
+            .handlaggningspecifikationId(handlaggning.handlaggningspecifikationId())
+            .build();
+   }
+
+   private IndividYrkandeRoll createIndividYrkandeRoll(String personnummer)
+   {
+      var individ = ImmutableIdtyp.builder()
+            .typId(PERSONNUMMER_TYP_ID)
+            .varde(personnummer)
+            .build();
+
+      return ImmutableIndividYrkandeRoll.builder()
+            .individ(individ)
+            .yrkandeRollId(UUID.randomUUID().toString())
+            .build();
+   }
+}
---
+++ src/test/java/se/fk/github/maskinellregelratttillforsakring/RtfKompletteringSvarServiceTest.java
@@ -0,0 +1,68 @@
+package se.fk.github.maskinellregelratttillforsakring;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import se.fk.github.maskinellregelratttillforsakring.logic.RtfKompletteringSvarService;
+import se.fk.rimfrost.framework.handlaggning.model.ImmutableHandlaggning;
+import se.fk.rimfrost.framework.handlaggning.model.ImmutableYrkande;
+import se.fk.rimfrost.regel.rtf.maskinell.jaxrsspec.controllers.generatedsource.model.RtfKompletteringSvar;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static se.fk.github.maskinellregelratttillforsakring.RtfMaskinellTestData.newRegelMaskinellRequest;
+
+class RtfKompletteringSvarServiceTest
+{
+
+   private final RtfKompletteringSvarService svarService = new RtfKompletteringSvarService();
+
+   @Test
+   void should_return_empty_svar_data()
+   {
+      var handlaggningUtanIndivider = ImmutableHandlaggning.builder()
+            .from(newRegelMaskinellRequest("19990101-1234").handlaggning())
+            .yrkande(ImmutableYrkande.builder()
+                  .from(newRegelMaskinellRequest("19990101-1234").handlaggning().yrkande())
+                  .individYrkandeRoller(List.of())
+                  .build())
+            .build();
+
+      var svarData = svarService.readSvarData(handlaggningUtanIndivider);
+
+      assertEquals(new RtfKompletteringSvar(), svarData);
+   }
+
+   @Test
+   void should_add_individer_for_each_registered_personnummer()
+   {
+      var handlaggningUtanIndivider = ImmutableHandlaggning.builder()
+            .from(newRegelMaskinellRequest("19990101-1234").handlaggning())
+            .yrkande(ImmutableYrkande.builder()
+                  .from(newRegelMaskinellRequest("19990101-1234").handlaggning().yrkande())
+                  .individYrkandeRoller(List.of())
+                  .build())
+            .build();
+
+      var svar = new RtfKompletteringSvar(List.of("19990101-1234", "19990101-3333"));
+
+      var update = svarService.registerSvar(handlaggningUtanIndivider, svar);
+
+      var individYrkandeRoller = update.yrkande().individYrkandeRoller();
+      assertEquals(2, individYrkandeRoller.size());
+      assertEquals("19990101-1234", individYrkandeRoller.get(0).individ().varde());
+      assertEquals("19990101-3333", individYrkandeRoller.get(1).individ().varde());
+      assertEquals("PERSONNUMMER", individYrkandeRoller.get(0).individ().typId());
+   }
+
+   @Test
+   void should_keep_existing_individer_when_adding_new_ones()
+   {
+      var handlaggning = newRegelMaskinellRequest("19990101-1234").handlaggning();
+      var befintligtAntal = handlaggning.yrkande().individYrkandeRoller().size();
+
+      var svar = new RtfKompletteringSvar(List.of("19990101-3333"));
+
+      var update = svarService.registerSvar(handlaggning, svar);
+
+      assertEquals(befintligtAntal + 1, update.yrkande().individYrkandeRoller().size());
+   }
+}
